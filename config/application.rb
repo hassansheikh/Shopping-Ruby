13a33371bd19770d97beb4edc9dadd281e470096
Hussnain <hassanejaz22@gmail.com>
2018-10-18 16:12:27 +0500

    production.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require "sass-rails" # add this here
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,10 @@ module Shopping
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+     config.sass.load_paths ||= []
+    config.sass.load_paths << "#{Rails.root}/app/assets/stylesheets"
+    config.sass.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
     
 
     # Settings in config/environments/* take precedence over those specified here.
