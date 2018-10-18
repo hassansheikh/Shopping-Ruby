2a699eb729339ef22e31794ce4b7f6172ac7e85b
Hussnain <hassanejaz22@gmail.com>
2018-10-18 17:28:53 +0500

    new update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   	@categories=Category.all
-  	@products=Product.where(:delete_status => 'not deleted',:status => 'active')
-  	@products_all=Product.where(:delete_status => 'not deleted',:status => 'in active')
+  	@products=Product.where(:delete_status => 'not deleted',:status => 'active').first(4)
+  	@products_all=Product.where(:delete_status => 'not deleted',:status => 'active').last(4)
   end
 end
