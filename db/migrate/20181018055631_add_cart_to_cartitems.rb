9d0211fe54a5549482e2f5a4fd3608dc434e92d0
Hussnain <hassanejaz22@gmail.com>
2018-10-19 17:04:45 +0500

    update

--- db/migrate/20181018055631_add_cart_to_cartitems.rb
+++ db/migrate/20181018055631_add_cart_to_cartitems.rb
@@ -1,5 +1,5 @@
 class AddCartToCartitems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :cartitems, :Cart, foreign_key: true
+    add_reference :cartitems, :cart, foreign_key: true
   end
 end
